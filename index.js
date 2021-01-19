e54b1e8d45ca6c783c5771e569623e813945ffd3
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-01-19 12:44:52 +0100

    feat: returns base64 string instead of 0

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const uuidv4 = require("uuid").v4;
 /**
  * @description
  * Write a random waifu image file on file system. Default to an uuid filename & root path (of parent process).
- * You can also pass an option Object with custom 'name' and 'path' values.
+ * You can also pass an option Object with custom 'filename' and 'path' values.
  *
  * @param {Object} options - OPTIONAL: pass Option object as parameter
  * @param {string} options.filename - Pass some string to chose an arbitrary filename
  * @param {string} options.path - Pass some path (relative to parent process). Ex: './__TESTS__/images'
  * @returns {Promise.<Error|0>}
- *    A promise that will either Resolve with 0 when file is successfully written, or Reject Error object otherwise.
+ *    A promise that will either Resolve with base64 string representation of the image when file is successfully written, 
+ *    and otherwise Reject Error object.
  * @example
  *     generateWaifu({path: "/images", filename:"megumin"})
- *       .then(res => console.log(res)) // 0
+ *       .then(res => console.log(res)) // data:image/png;base64,/9j/base64encodedblahblah
  *       .catch(err => console.error(err))
  */
 const generateWaifu = async (options) =>
@@ -25,16 +26,16 @@ const generateWaifu = async (options) =>
 
     const handleOptions = () => {
       if (filename && path) {
-        return `${path}/${filename}.jpg`
+        return `${path}/${filename}.png`
       }
       else if (filename && !path) {
-        return `${filename}.jpg`
+        return `${filename}.png`
       }
       else if (!filename && path) {
-        return `${path}/${randomNumber}_${uuidv4()}.jpg`
+        return `${path}/${randomNumber}_${uuidv4()}.png`
       }
       else {
-        return `${randomNumber}_${uuidv4()}.jpg`
+        return `${randomNumber}_${uuidv4()}.png`
       }
     }
 
@@ -43,12 +44,11 @@ const generateWaifu = async (options) =>
       res.on("error", (e) => reject(e));
       res.pipe(fs.createWriteStream(options));
       res.on("end", async () => {
-        const response = await fs.promises.readFile(`./${options}`, { encoding: "base64" })
-        console.log("response? ", response)
-        return resolve(response)
+        const base64str = await fs.promises.readFile(`./${options}`, { encoding: "base64" })
+        return resolve(`data:image/png;base64,${base64str}`)
       }
       );
     });
   })
 
-generateWaifu();
+module.exports = generateWaifu;
